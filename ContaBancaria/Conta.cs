8cb699563acecc138d279068ca8de65b6ce7d5e7
Charleu <sdlimeira0@gmail.com>
2024-09-10 00:54:09 -0300

    Primeiro commit

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        public int NumeroConta { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }
+        
+        private double Taxa = 5.00;
+        public Conta(int numeroConta, string nome)
+        {
+            NumeroConta = numeroConta;
+            Nome = nome;
+        }
+
+        public Conta(int numeroConta, string nome, double depositoInicial) : this(numeroConta, nome)
+        {
+            Depositar(depositoInicial);
+        }
+
+        public void Depositar(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Sacar(double quantia)
+        {
+            Saldo -= quantia + Taxa;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"\nConta: {NumeroConta}\nTitular: {Nome}\nSaldo atual: {Saldo.ToString("C")}");
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ContaBancaria
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            double depositoInicial = 0;
+            string resposta;
+
+            do
+            {
+                Console.WriteLine("Deseja fazer um depósito inicial? (Digite '1' para SIM ou '0' para NÃO):");
+                resposta = Console.ReadLine();
+
+                if (resposta == "1")
+                {
+                    Console.WriteLine("Digite o valor do depósito inicial:");
+                    depositoInicial = double.Parse(Console.ReadLine());
+                }
+                else if (resposta == "0")
+                {
+                    Console.WriteLine("Nenhum depósito inicial será realizado.");
+                }
+                else
+                {
+                    Console.WriteLine("Opção inválida. Por favor, digite '1' para SIM ou '0' para NÃO.");
+                }
+
+            } while (resposta != "1" && resposta != "0");
+
+            Conta conta = new Conta(0100934, "Charleu", depositoInicial);
+            conta.ExibirDados();
+
+            Console.WriteLine("\nDigite uo valor depósito:");
+            conta.Depositar(double.Parse(Console.ReadLine()));
+            conta.ExibirDados();
+            Console.WriteLine("\nDigite o valor para saque:");
+            conta.Sacar(double.Parse(Console.ReadLine()));
+            conta.ExibirDados();
+            Console.ReadKey();
+        }
+    }
+}
